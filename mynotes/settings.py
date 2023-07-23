dba7e62ed2eecd10aa55b29ba415088ffde88faf
DeDomin98 <75976221+DeDomin98@users.noreply.github.com>
2023-07-23 14:49:44 +0200

    Update settings.py

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -140,11 +140,14 @@ WSGI_APPLICATION = 'mynotes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',                      
+        'USER': 'postgres',
+        'PASSWORD': 'I3CdpoRarojut0qwAU0s',
+        'HOST': 'containers-us-west-117.railway.app',
+        'PORT': '6169',
+    }
 }
-
 
  
 
